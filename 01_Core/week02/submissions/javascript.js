baeddc690678a5ee8cec7a121fed1bb57256b9f6
Cassidy Rase <crase@c02yj3bfjg5j.redventures.net>
2019-06-24 09:51:14 -0400

    Adding project to branch

---
+++ 01_Core/week02/submissions/javascript.js
@@ -0,0 +1,16 @@
+var names = ["John Doe", "Mike Smith", "Gary Thompson", "Thomas Williams", "Joe Miller"];
+console.log(names.sort());
+/*
+document.getElementById("dos").innerHTML = names;
+*/
+
+
+
+
+/*
+getElementById('page-two');
+
+var = [John Doe, Mike Smith, Gary Thompson, Thomas Williams, Joe Miller];
+
+function makeAlphabet(str)
+*/
